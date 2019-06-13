9a532bb86e06b6ff0fde42b00ab715b7b020b5a7
Pankaj Bhalala <pankaj.bhalala.sa@gmail.com>
2019-06-13 12:18:27 +0530

    Update for screenshots

---
+++ SATextfield/SATextfield/SATextfield.h
@@ -0,0 +1,19 @@
+//
+//  SATextfield.h
+//  SATextfield
+//
+//  Created by Pankaj Bhalala on 13/06/19.
+//  Copyright © 2019 Solution Analysts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SATextfield.
+FOUNDATION_EXPORT double SATextfieldVersionNumber;
+
+//! Project version string for SATextfield.
+FOUNDATION_EXPORT const unsigned char SATextfieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SATextfield/PublicHeader.h>
+
+
--- SATextfield/SATextfieldEx/Controller/ViewController.swift
+++ SATextfield/SATextfieldEx/Controller/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+//import SATextfield
 
 class ViewController: UIViewController, SATextfieldDelegate {
 
